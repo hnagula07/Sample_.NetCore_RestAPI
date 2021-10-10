04f2bd2242a24f0c7de39c6ea29b71a1a7d86237
hnagula <hnagula@ezesoft.com>
2021-10-10 16:42:47 +0530

    Used List, Working Model covered Special cases

--- SampleAPI/Services/PatientGroup_Service.cs
+++ SampleAPI/Services/PatientGroup_Service.cs
@@ -10,9 +10,10 @@ namespace SampleAPI.Services
     {
         
         HashSet<string> Visited = new HashSet<string>();
+        int ColumnsCount=0;
         public Patient CalculatePatientGroups(PatientData pp){
              
-            int columnsCount= pp.matrix[0].Count;
+           ColumnsCount = pp.matrix[0].Count;
             int row = 0;
             int result = 0;
             foreach (var itemArrays in pp.matrix)
@@ -25,11 +26,9 @@ namespace SampleAPI.Services
                 
                 foreach (var item in itemArrays)
                 {
-                      if (ValidatePatients(item,row,column)) {
-                          Visited.Add(row.ToString()+column.ToString());
+                      if (ValidatePatients(item,row,column,pp)) {
                           result++;
                       }
-                 
                     column++;
                 }
                 row++;
@@ -41,22 +40,41 @@ namespace SampleAPI.Services
             
         }
 
-        private bool ValidatePatients(int patient,int row,int column){
+        private bool ValidatePatients(int patient,int row,int column,PatientData pp){
             if(patient == 0){
                 return false;
             }
-            List<int> coOrdinates = new List<int>();
-            // Conditions
-            if(row==0){
-
+            Visited.Add(row.ToString()+column.ToString());
+            List<string> coOrdinates = new List<string>();
+    
+            if(row==0 && column>0){
+                if(CheckForSpecialCase(row,column,pp)){
+                    return false;
+                }
+                coOrdinates.Add(row.ToString()+(column-1).ToString());
+            }else if(row>0){
+            coOrdinates.Add((row-1).ToString()+(column-1).ToString()); //can skip
+            coOrdinates.Add((row-1).ToString()+(column).ToString());
+            coOrdinates.Add((row-1).ToString()+(column+1).ToString()); //can skip
+            }else if(column>0){
+                coOrdinates.Add(row.ToString()+(column-1).ToString());
             }
 
-            if(Visited.Contains(row.ToString()+column.ToString())){
-
+            foreach(var index in coOrdinates){
+                if(Visited.Contains(index)){
+                    return false;
+                }
             }
-
             return true;
         }
         
+        private bool CheckForSpecialCase(int row,int column,PatientData pp){
+            if(Visited.Contains(row.ToString()+(column-2).ToString())){
+                    if(row+1 <= pp.matrix.Count && column-1 >=0 && pp.matrix[row+1][column-1] == 1){
+                        return true;
+                    }
+            }
+            return false;
+        }
     }
 }
