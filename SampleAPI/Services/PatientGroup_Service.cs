d1956227710a2ef126a8590d8fd3f429a958c3d8
hnagula <hnagula@ezesoft.com>
2021-10-10 22:16:54 +0530

    Added Map to store NodesGroup

--- SampleAPI/Services/PatientGroup_Service.cs
+++ SampleAPI/Services/PatientGroup_Service.cs
@@ -9,7 +9,8 @@ namespace SampleAPI.Services
     public class PatientGroup_Service
     {
         
-        HashSet<string> Visited = new HashSet<string>();
+        Dictionary<string,int> Visited = new Dictionary<string,int>();
+        Dictionary<int,List<string>> NodesGroup = new Dictionary<int,List<string>>
         int ColumnsCount=0;
         public Patient CalculatePatientGroups(PatientData pp){
              
@@ -28,6 +29,8 @@ namespace SampleAPI.Services
                 {
                       if (ValidatePatients(item,row,column,pp)) {
                           result++;
+                          Visited.Add(row.ToString()+column.ToString(),result);
+                          NodesGroup.Add(result,new List<string>{row.ToString()+column.ToString()})
                       }
                     column++;
                 }
@@ -41,31 +44,43 @@ namespace SampleAPI.Services
         }
 
         private bool ValidatePatients(int patient,int row,int column,PatientData pp){
+            bool result = false;
             if(patient == 0){
-                return false;
+                return result;
             }
-            Visited.Add(row.ToString()+column.ToString());
+            Visited.Add(row.ToString()+column.ToString(),0);
             List<string> coOrdinates = new List<string>();
     
             if(row==0 && column>0){
-                if(CheckForSpecialCase(row,column,pp)){
-                    return false;
-                }
+                // if(CheckForSpecialCase(row,column,pp)){
+                //     return result;
+                // }
                 coOrdinates.Add(row.ToString()+(column-1).ToString());
             }else if(row>0){
-            coOrdinates.Add((row-1).ToString()+(column-1).ToString()); //can skip
-            coOrdinates.Add((row-1).ToString()+(column).ToString());
-            coOrdinates.Add((row-1).ToString()+(column+1).ToString()); //can skip
+                coOrdinates.Add((row-1).ToString()+(column-1).ToString()); //can skip
+                coOrdinates.Add((row-1).ToString()+(column).ToString());
+                coOrdinates.Add((row-1).ToString()+(column+1).ToString()); //can skip
             }else if(column>0){
                 coOrdinates.Add(row.ToString()+(column-1).ToString());
             }
 
+            HashSet<int> paths = new HashSet<int>();
+
             foreach(var index in coOrdinates){
                 if(Visited.Contains(index)){
-                    return false;
+                    int rank = Visited[index];
+                    string index = row.ToString()+column.ToString();
+                    Visited[index] = rank;
+                    NodesGroup[rank] =  NodesGroup[rank].Add(index);
+                   paths.Add(rank);
                 }
             }
-            return true;
+            if (paths.Count == 0){
+                result = true
+            }else{
+                
+            }
+            return result;
         }
         
         private bool CheckForSpecialCase(int row,int column,PatientData pp){
@@ -73,7 +88,10 @@ namespace SampleAPI.Services
                     if(row+1 <= pp.matrix.Count && column-1 >=0 && pp.matrix[row+1][column-1] == 1){
                         return true;
                     }
-            }
+            }else if(Visited.Contains(row.ToString()+(column-2).ToString())){
+                    if(row+1 <= pp.matrix.Count && column-1 >=0 && pp.matrix[row+1][column-1] == 1){
+                        return true;
+                    }
             return false;
         }
     }
